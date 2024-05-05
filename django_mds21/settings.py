c0f24c729dbe04e4d4e0e540fdf1b4f37c7e8f5b
KaiKiat <113549970+TeoJieRu@users.noreply.github.com>
2024-05-05 13:10:55 +0800

    feat: initialize django rest api

--- django_mds21/settings.py
+++ django_mds21/settings.py
@@ -38,16 +38,24 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8080",
+    "http://localhost:8080",
 ]
 
 ROOT_URLCONF = 'django_mds21.urls'
